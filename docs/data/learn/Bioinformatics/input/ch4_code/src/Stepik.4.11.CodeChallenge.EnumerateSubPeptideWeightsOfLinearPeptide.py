7fccb97544f1148cd9b12ad19d79c376e520fd27
offbynull <offbynull@gmail.com>
2020-11-08 08:57:58 -0500

    More bioinf work

--- docs/data/learn/Bioinformatics/input/ch4_code/src/Stepik.4.11.CodeChallenge.EnumerateSubPeptideWeightsOfLinearPeptide.py
+++ docs/data/learn/Bioinformatics/input/ch4_code/src/Stepik.4.11.CodeChallenge.EnumerateSubPeptideWeightsOfLinearPeptide.py
@@ -1,25 +1,10 @@
-from Utils import get_amino_acid_to_mass_table
+from TheoreticalSpectrumOfLinearPeptide import theoretical_spectrum_of_linear_peptide
 
 with open('/home/user/Downloads/dataset_240286_2.txt', mode='r', encoding='utf-8') as f:
     data = f.read()
 
 data = data.strip()
 
-mass_table = get_amino_acid_to_mass_table()
+spectrum = theoretical_spectrum_of_linear_peptide(data)
 
-prefix_masses = [0]
-for i, ch in enumerate(data):
-    prev_mass = prefix_masses[i]
-    next_mass = prev_mass + mass_table[ch]
-    prefix_masses.append(next_mass)
-# print(f'{prefix_masses}')
-
-ret = [0]
-for k_end in range(0, len(prefix_masses)):
-    for k_start in range(0, k_end):
-        min_mass = prefix_masses[k_start]
-        max_mass = prefix_masses[k_end]
-        ret.append(max_mass - min_mass)
-
-ret.sort()
-print(f'{" ".join([str(i) for i in ret])}')
+print(f'{" ".join([str(i) for i in spectrum])}')
