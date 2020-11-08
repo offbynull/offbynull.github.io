dee05ad7e8920ee3ff8be37de821ebe9bfa85415
offbynull <offbynull@gmail.com>
2020-11-08 17:34:54 -0500

    More bioinf work

---
+++ docs/data/learn/Bioinformatics/output/ch4_code/src/Stepik.4.8.ExerciseBreak.LeaderboardCyclopeptideSequencingOnExtendedAminoAcidAlphabet.py
@@ -0,0 +1,17 @@
+from LeaderboardCyclopeptideSequencing import sequence_cyclic_peptide
+from Utils import get_unique_amino_acid_masses_as_dict
+
+with open('/home/user/Downloads/dataset_240283_2.txt', mode='r', encoding='utf-8') as f:
+    data = f.read()
+
+data = data.split('\n')
+n = int(data[0].strip())
+cyclic_peptide_experimental_spectrum = [int(w) for w in data[1].strip().split()]
+cyclic_peptide_experimental_spectrum.sort()  # should be sorted already, but just in case
+
+mass_table = dict([(i, i) for i in range(57, 200 + 1)])
+
+leader_peptides = sequence_cyclic_peptide(cyclic_peptide_experimental_spectrum, n, mass_table=mass_table)
+
+ret = ' '.join(['-'.join([str(i) for i in p]) for p in leader_peptides])
+print(f'{ret}')
