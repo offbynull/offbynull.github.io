81278c73b951734798649a05ea10809dbf302d03
offbynull <offbynull@gmail.com>
2020-12-30 19:32:54 -0500

    More bioinf work

---
+++ docs/data/learn/Bioinformatics/output/ch5_code/src/Stepik.5.12.ExerciseBreak.NumberOfEdgesIn3LevelGraph.py
@@ -0,0 +1,37 @@
+# Exercise Break: Prove that the number of edges in the three-level graph described in the figure below is at most
+# 7 · n · m for sequences of length n and m.
+#
+#
+#
+# Given a 6x6 graph (n=6, m=6)...
+#
+# lower layer has 6*5 edges
+# middle layer has 5*5 edges
+# upper layer has 5*6 edges
+# middle layer to lower layer edges has 5*5
+# lower layer to middle layer edges has 5*5
+# middle layer to upper layer edges has 5*5
+# upper layer to middle layer edges has 5*5
+#
+# So abstract it out: Given an nxm graph...
+#
+# lower layer has n*m-1 edges
+# middle layer has n-1*m-1 edges
+# upper layer has n-1*m edges
+# middle layer to lower layer edges has n-1*m-1
+# lower layer to middle layer edges has n-1*m-1
+# middle layer to upper layer edges has n-1*m-1
+# upper layer to middle layer edges has n-1*m-1
+#
+# n*m-1 + n-1*m-1 + n-1*m + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 <= 7*n*m
+# n*m-1 - n + n-1*m-1 + n-1*m + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 <= 7*n*m - n
+# n-1*m-1 + n-1*m-1 + n-1*m + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 <= 7*n*m - n
+# n-1*m-1 + n-1*m-1 + n-1*m - n-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 <= 7*n*m - n - n - 1
+# n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 + n-1*m-1 <= 7*n*m - n - n - 1
+# 7*n-1*m-1 <= 7*n*m - n - n - 1
+# 7*n-1*m-1 <= 7*n*m - 2n - 1
+
+# I hope I didn't screw up the reasoning somewhere, but I think this is correct. For n and m of ...
+# 0, it'll be 0 <= 0
+# 1, it'll be 0 <= 5
+# 2, it'll be 7 <= 23
