6f19b545dd039fa14f3a8bd4cea232646c001545
offbynull <offbynull@gmail.com>
2020-12-31 15:20:42 -0500

    More bioinf work

---
+++ docs/data/learn/Bioinformatics/output/ch5_code/src/Stepik.5.13.ExerciseBreak.FindAllMiddleNodes.py
@@ -0,0 +1,46 @@
+# Input 1 = ACCA
+# Input 2 = CAAC
+#
+#    C A A C
+#   o-o o o o
+#  A   \
+#   o o o-o o
+#  C       \
+#   o o o o o
+#  C        |
+#   o o o o o
+#  A        |
+#   o o o o o
+#
+#    C A A C
+#   o o o o o
+#  A|
+#   o o o o o
+#  C \
+#   o o-o-o o
+#  C       \
+#   o o o o o
+#  A        |
+#   o o o o o
+#
+#
+# The above 2 graphs are the 2 paths for hte longest common subsequence I think? The middle nodes for the longest paths
+# are...
+#
+#    C A A C
+#   o o o o o
+#  A
+#   o o X o o
+#  C
+#   o o X o o
+#  C
+#   o o o o o
+#  A
+#   o o o o o
+#       ^
+#       |
+#  the nodes traversed through the
+#  middle column are marked with X
+#
+#
+# There is no code for this. They asked you to do this just by looking at it.
